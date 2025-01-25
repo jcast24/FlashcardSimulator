b16d506a2f41b283847c5e546865fa9131ba3402
Jesus Castro <castrojesus39@gmail.com>
2025-01-24 17:40:48 -0800

    initialized UserInterface class into Program.cs

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        UserInterface ui = new UserInterface();
+        ui.MainMenu();
     }
 }
