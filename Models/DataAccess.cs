a028b3f81e02687cd80d5f0bc3bd07f24d8b7634
Jesus Castro <castrojesus39@gmail.com>
2025-01-25 22:57:49 -0800

    added actual db connection + created CreateTables method to make tables for both stack and flashcards

--- Models/DataAccess.cs
+++ Models/DataAccess.cs
@@ -17,21 +17,43 @@ public class DataAccess
         ConnectionString = config.GetSection("ConnectionStrings")["DefaultConnection"];
     }
 
-    public bool TestConnection()
+    internal void CreateTables() 
     {
         try
         {
-            using (var connection = new SqlConnection(ConnectionString))
+            using (var conn = new SqlConnection(ConnectionString))
             {
-                connection.Open();
-                var result = connection.QuerySingle<int>("SELECT 1");
-                return result == 1;
+                conn.Open();
+
+                string createStackTableSql = 
+                    @"IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Stacks')
+                    CREATE TABLE Stacks (
+                            Id int IDENTITY(1,1) NOT NULL,
+                            Name NVARCHAR(30) NOT NULL UNIQUE,
+                            PRIMARY KEY (Id)
+                            );";
+                conn.Execute(createStackTableSql);
+
+                string createFlashCardTableSql =
+                    @"IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Flashcards')
+                    CREATE TABLE Flashcards (
+                        Id int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+                        Question NVARCHAR(30) NOT NULL,
+                        Answer NVARCHAR(30) NOT NULL,
+                        StackId int NOT NULL
+                            FOREIGN KEY
+                            REFERENCES Stacks(Id)
+                            ON DELETE CASCADE
+                            ON UPDATE CASCADE
+                    );";
+
+                conn.Execute(createFlashCardTableSql);
+                Console.WriteLine("Connection successful!");
             }
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Connection failed: {ex.Message}");
-            return false;
+        } catch (Exception ex) {
+            Console.WriteLine($"Error: {ex.Message}");
         }
     }
+
+
 }
