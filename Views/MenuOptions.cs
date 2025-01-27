1a80ee9b64b21d0e3b4563912c8bd92bf5197f69
Jesus Castro <castrojesus39@gmail.com>
2025-01-26 22:00:08 -0800

    created file that contains the menu options for both stacks/flashcards using structs (might change back to enums)

---
+++ Views/MenuOptions.cs
@@ -0,0 +1,23 @@
+namespace FlashcardSimulator;
+
+internal struct Menu
+{
+    public const string StacksMenu = "Manage Stacks";
+    public const string FlashcardMenu = "Manage Flashcard Menu";
+    // public const string StudyArea = "Study Session";
+    public const string Quit = "Quit";
+}
+
+internal struct StacksMenu
+{
+    public const string CreateNewStack = "Create new stack";
+    public const string ManageAllStacks = "List all Stacks";
+    public const string DeleteStack = "Delete Stack";
+}
+
+internal struct FlashcardMenu
+{
+    public const string CreateNewFlashcard = "Create new flashcard";
+    public const string UpdateFlashcard = "Update existing flashcard";
+    public const string DeleteFlashcard = "Delete Flashcard";
+}
