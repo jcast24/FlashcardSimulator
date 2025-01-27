2cb3aa8abfe40f68f360bc9c3e36cf945404a450
Jesus Castro <castrojesus39@gmail.com>
2025-01-26 22:06:36 -0800

    redid the menu options which is going to include two new menus for stacks and flashcards

--- Views/UserInterface.cs
+++ Views/UserInterface.cs
@@ -6,34 +6,66 @@ public class UserInterface()
 {
     internal void MainMenu()
     {
-        string[] menuChoices = new string[3]
-        {
-            "Create a new stack",
-            "List all stacks",
-            "Delete a stack",
-        };
-
-        while (true)
+        bool isRunning = true;
+        while (isRunning)
         {
             Console.Clear();
 
             var choice = AnsiConsole.Prompt(
-                new SelectionPrompt<string>().Title("Pick a choice!").AddChoices(menuChoices)
+                new SelectionPrompt<string>()
+                    .Title("Pick a choice!")
+                    .AddChoices(Menu.StacksMenu, Menu.FlashcardMenu, Menu.Quit)
+            );
+
+            switch (choice)
+            {
+                case Menu.StacksMenu:
+                    Console.Write("This goes to the stacks menu");
+                    break;
+                case Menu.FlashcardMenu:
+                    Console.Write("List all stacks method goes here");
+                    break;
+                case Menu.Quit:
+                    Console.Write("Goodbye!");
+                    isRunning = false;
+                    break;
+                default:
+                    Console.Write("Please choose an option!");
+                    break;
+            }
+        }
+    }
+
+    internal static void MenuStack()
+    {
+        Console.Clear();
+
+        bool isRunning = true;
+        while (isRunning)
+        {
+            var choice = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .Title("Stacks menu!")
+                    .AddChoices(
+                        StacksMenu.ManageAllStacks,
+                        StacksMenu.CreateNewStack,
+                        StacksMenu.DeleteStack
+                    )
             );
 
             switch(choice)
             {
-                case "Create a new stack":
-                    Console.Write("Create new stack method goes here");
+                case StacksMenu.ManageAllStacks:
+                    Console.Write("List all the stacks here");
                     break;
-                case "List all stacks":
-                    Console.Write("List all stacks method goes here");
+                case StacksMenu.CreateNewStack:
+                    Console.Write("Add method to create a new stack, once a new stack is made ask to create a new flashcard");
                     break;
-                case "Delete a stack":
-                    Console.Write("Delete a stack");
+                case StacksMenu.DeleteStack:
+                    Console.Write("Delete stack by name");
                     break;
                 default:
-                    Console.Write("Please choose an option!");
+                    Console.Write("Please choose the correct option!");
                     break;
             }
         }
