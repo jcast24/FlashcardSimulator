3a6cb292852997e0ebf59965425c9df13bbf44c1
Jesus Castro <castrojesus39@gmail.com>
2025-01-26 22:11:35 -0800

    added instance of the user interface

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@ class Program
     static void Main(string[] args)
     {
         DataAccess db = new DataAccess();
+        UserInterface ui = new UserInterface();
         db.CreateTables();
+        ui.MainMenu();
     }
 }
