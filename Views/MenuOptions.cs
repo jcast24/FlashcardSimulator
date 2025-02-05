5103154410f990eff2521f9bf4493c291a0caa19
Jesus Castro <castrojesus39@gmail.com>
2025-02-04 21:00:16 -0800

    added return to menu for flashcard

--- Views/MenuOptions.cs
+++ Views/MenuOptions.cs
@@ -21,4 +21,5 @@ internal struct FlashcardMenu
     public const string CreateNewFlashcard = "Create new flashcard";
     public const string UpdateFlashcard = "Update existing flashcard";
     public const string DeleteFlashcard = "Delete Flashcard";
+    public const string ReturnToMenu = "Return to Main Menu";
 }
