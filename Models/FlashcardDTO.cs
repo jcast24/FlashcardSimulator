83f567ae5f229742af7b0e62d3fb891c9bf6a1fe
Jesus Castro <castrojesus39@gmail.com>
2025-02-13 22:14:52 -0800

    added String.Empty values to FlashcardDTO to avoid null

--- Models/FlashcardDTO.cs
+++ Models/FlashcardDTO.cs
@@ -1,7 +1,7 @@
 public class FlashcardDTO
 {
     public int FlashcardId { get; set; }
-    public string Question { get; set; }
-    public string Answer { get; set; }
-    public string StackName { get; set; }
+    public string Question { get; set; } = String.Empty;
+    public string Answer { get; set; } = String.Empty;
+    public string StackName { get; set; } = String.Empty;
 }
