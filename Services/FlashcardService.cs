ee2fbe17e45184082f08cebdb127968b335bd8be
Jesus Castro <castrojesus39@gmail.com>
2025-03-21 22:19:55 -0700

    completely overhauled Flashcard Service, but still need to do Delete method

--- Services/FlashcardService.cs
+++ Services/FlashcardService.cs
@@ -2,236 +2,129 @@ using Dapper;
 using Microsoft.Data.SqlClient;
 using Spectre.Console;
 
-namespace FlashcardSimulator;
+namespace FlashcardSimulator.Services;
 
 class FlashcardService
 {
     private readonly DataAccess _dataAccess;
+    private readonly StackService _stackService;
 
-    public FlashcardService(DataAccess dataAccess)
+    public FlashcardService(DataAccess dataAccess, StackService stackService)
     {
         _dataAccess = dataAccess;
+        _stackService = stackService;
     }
 
-    // has to return a string
-    internal string GetFlashcardsWithStackNames()
+    // Get All Flashcards
+    internal List<Flashcard> GetAllFlashcards()
     {
         try
         {
-            using (var conn = new SqlConnection(_dataAccess.GetConnection()))
-            {
-                string sql =
-                    "SELECT FlashcardId as Id, Question, Answer, StackName FROM FlashcardView;";
-                var records = conn.Query<FlashcardDTO>(sql).ToList();
+            using var connection = new SqlConnection(_dataAccess.GetConnection());
+            connection.Open();
+            string selectQuery = "SELECT * FROM Flashcards ORDER BY Id";
+            var records = connection.Query<Flashcard>(selectQuery).ToList();
 
-                foreach (var rec in records)
-                {
-                    return $"{rec.StackName}";
-                }
-            }
+            return records;
         }
-        catch (Exception ex)
+        catch (Exception e)
         {
-            AnsiConsole.Write($"[red]{ex.Message}[/]");
+            Console.WriteLine($"Error: {e.Message}");
+            return new List<Flashcard>();
         }
-        return "Not found";
     }
 
-    // Change this, choose the stack by stack name, not by the Id of the stack
-    // Do this because we want to show the stack name when we show all the flashcards
-    private int ChooseStackById()
+    // Show All Flashcards (this is for the menu)
+    internal void ShowAllFlashcards()
     {
-        // List<Stack> stacks = _dataAccess.GetAllStacks();
-        // var stacksArray = stacks.Select(x => x.Name).ToArray();
-        //
-        // var option = AnsiConsole.Prompt(
-        //     new SelectionPrompt<string>().Title("Choose a stack: ").AddChoices(stacksArray)
-        // );
-        //
-        // var stackId = stacks.Single(x => x.Name == option).Id;
-        // return stackId;
-        
-        List<Stack> stacks = _dataAccess.GetAllStacks();
+        var getAllFlashcards = GetAllFlashcards();
 
-        Dictionary<string, int> stackDict = stacks.ToDictionary(s => s.Name, s => s.Id);
+        if (!getAllFlashcards.Any())
+        {
+            AnsiConsole.MarkupLine("[red]There are no flashcards available[/]");
+        }
 
-        string option = AnsiConsole.Prompt(
-                new SelectionPrompt<string>().Title("Choose stack: ").AddChoices(stackDict.Keys)
-                );
-        
-        // option: (string equivalent)
-        // "Cooking" : 1
-        // "Pottery" : 2
-        // "Another option goes here" : 3
-
-        int stackId = stackDict[option]; // this is basically stackDict["Cooking"] = 1 <- The id etc etc
-
-        return stackId;
+        foreach (var flashcard in getAllFlashcards)
+        {
+            AnsiConsole.MarkupLine($"ID: {flashcard.Id}\nQuestion: {flashcard.Question}\nAnswer: {flashcard.Answer}");
+        }
     }
 
+    // Get Flashcard by ID
+    internal int GetFlashcardById()
+    {
+        var flashcards = GetAllFlashcards();
+        var flashcardsArray = flashcards.Select(x => x.Question).ToArray();
+
+        var option = AnsiConsole.Prompt(new SelectionPrompt<string>()
+            .Title("Choose a flashcard: ")
+            .AddChoices(flashcardsArray));
+
+        var flashcardId = flashcards.Single(x => x.Question == option).Id;
+        return flashcardId;
+    }
+
+
+    // Create a new Flashcard
     internal void CreateAFlashcard()
     {
-        // List all the stacks
-        // choose which stack based off of ID
-        Flashcard flashcard = new();
+        Flashcard newFlashcard = new();
+        newFlashcard.StackId = _stackService.ChooseStackById();
 
-        // Connect the stackID from Stack model to Flashcard model
-        flashcard.StackId = ChooseStackById();
+        newFlashcard.Question = AnsiConsole.Ask<string>("What is your question? ");
 
-        flashcard.Question = AnsiConsole.Ask<string>("Enter your question: ");
-        flashcard.Answer = AnsiConsole.Ask<string>("Enter your answer: ");
+        while (string.IsNullOrEmpty(newFlashcard.Question))
+        {
+            newFlashcard.Question = AnsiConsole.Ask<string>("Question can't be empty. Try again: ");
+        }
 
-        DataAccess data = new DataAccess();
-        InsertFlashcardIntoStack(flashcard);
+
+        newFlashcard.Answer = AnsiConsole.Ask<string>("What is your answer? ");
+
+        while (string.IsNullOrEmpty(newFlashcard.Answer))
+        {
+            newFlashcard.Answer = AnsiConsole.Ask<string>("Question can't be empty. Try again: ");
+        }
+
+        InsertFlashcardIntoStack(newFlashcard);
     }
 
-    // Instead of stackId, should be better to use the name of the stack.
+    // Insert into Database
     internal void InsertFlashcardIntoStack(Flashcard flashcard)
     {
-        DataAccess db = new DataAccess();
         try
         {
-            using (var conn = new SqlConnection(_dataAccess.GetConnection()))
-            {
-                conn.Open();
-
-                string insertFlashcardQuery =
-                    @"INSERT INTO Flashcards (Question, Answer, StackId) VALUES (@Question, @Answer, @StackId)";
-                conn.Execute(
-                    insertFlashcardQuery,
-                    new
-                    {
-                        flashcard.Question,
-                        flashcard.Answer,
-                        flashcard.StackId,
-                    }
-                );
-            }
+            using var connection = new SqlConnection(_dataAccess.GetConnection());
+            connection.Open();
+            string insertQuery =
+                "INSERT INTO Flashcards (Question, Answer, StackId) VALUES (@Question, @Answer, @StackId)";
+            connection.Execute(insertQuery, new { flashcard.Question, flashcard.Answer, flashcard.StackId });
+            AnsiConsole.Markup($"[green]Successfully added flashcard into stack[/]");
         }
-        catch (Exception ex)
+        catch (Exception e)
         {
-            Console.WriteLine($"{ex.Message}");
+            AnsiConsole.MarkupLine($"[red]Error InsertFlashcardIntoStack: {e.Message}[/]");
         }
+
+        AnsiConsole.MarkupLine("[cyan]Press any key to continue[/]");
+        Console.ReadKey();
     }
 
-    // Add input validation when deleting
-    // if I do delete flashcard 5 and 5 isn't there then it should give me
-    // "flashcard 5 does not exist"
-    // UPDATED VERSION CHECK THIS AGAIN (2/13/2025)
-    internal void DeleteAFlashcard()
-    {
-        ListAllFlashcards();
-        try
-        {
-            using (var conn = new SqlConnection(_dataAccess.GetConnection()))
-            {
-                int getId = AnsiConsole.Ask<int>("Enter the ID of the item to delete: ");
-
-                int itemExists = conn.ExecuteScalar<int>(
-                    "SELECT COUNT(1) FROM Flashcards WHERE Id = @Id",
-                    new { Id = getId }
-                );
-
-                if (itemExists == 0)
-                {
-                    AnsiConsole.MarkupLine("[red] Item does not exist! [/]");
-                }
-                else
-                {
-                    int rowsAffected = conn.Execute(
-                        "DELETE From Flashcards WHERE Id = @Id",
-                        new { Id = getId }
-                    );
-                    AnsiConsole.MarkupLine("[green]Item has been successfully deleted![/]");
-                }
-            }
-        }
-        catch (Exception ex)
-        {
-            AnsiConsole.MarkupLine($"[red]{ex.Message}[/]");
-        }
-    }
-
-    internal void ListAllFlashcards()
-    {
-        Console.Clear();
-        try
-        {
-            using (var conn = new SqlConnection(_dataAccess.GetConnection()))
-            {
-                conn.Open();
-
-                string listFlashcardsQuery = "SELECT * FROM FlashcardView";
-
-                var records = conn.Query<FlashcardDTO>(listFlashcardsQuery).ToList();
-
-                if (records.Count == 0)
-                {
-                    AnsiConsole.MarkupLine("[red]No flashcards currently exist.[/]");
-                }
-
-                foreach (var item in records)
-                {
-                    Console.WriteLine(
-                        $"{item.StackName} - {item.FlashcardId} - {item.Question} - {item.Answer}"
-                    );
-                }
-            }
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"{ex.Message}");
-        }
-    }
-
+    // Update a flashcard based on id
     internal void UpdateFlashcard()
     {
         Console.Clear();
-        ListAllFlashcards();
+        
+        using var connection = new SqlConnection(_dataAccess.GetConnection());
 
-        using (var conn = new SqlConnection(_dataAccess.GetConnection()))
-        {
-            bool doesItemExist;
-            int getId;
-            do
-            {
-                getId = AnsiConsole.Ask<int>("Enter the ID of the flashcard to update: ");
+        int getId = GetFlashcardById();
+        
+        var updatedQuestion = AnsiConsole.Ask<string>("Re-enter question: ");
+        var updatedAnswer = AnsiConsole.Ask<string>("Re-enter answer: ");
 
-                doesItemExist =
-                    conn.ExecuteScalar<int>(
-                        "SELECT COUNT(1) FROM Flashcards WHERE Id = @Id",
-                        new { Id = getId }
-                    ) > 0;
-
-                if (!doesItemExist)
-                {
-                    AnsiConsole.MarkupLine("[red]Item does not exist! Please enter a valid ID.[/]");
-                }
-            } while (!doesItemExist);
-
-            string updatedQuestion = AnsiConsole.Ask<string>("Re-enter the question: ");
-            string updatedAnswer = AnsiConsole.Ask<string>("Re-enter the answer: ");
-
-            string updatedQuery =
-                "UPDATE Flashcards SET Question=@Question, Answer=@Answer WHERE Id = @Id";
-
-            int rowsAffected = conn.Execute(
-                updatedQuery,
-                new
-                {
-                    Question = updatedQuestion,
-                    Answer = updatedAnswer,
-                    Id = getId,
-                }
-            );
-            if (rowsAffected > 0)
-            {
-                AnsiConsole.MarkupLine("[green]Item has been successfully updated[/]");
-            }
-            else
-            {
-                AnsiConsole.MarkupLine("[red]Update failed. Please try again![/]");
-            }
-        }
+        string updateQuery = "UPDATE Flashcards SET Question=@Question, Answer=@Answer WHERE Id=@Id";
+        connection.Execute(updateQuery, new { Question = updatedQuestion, Answer = updatedAnswer, Id = getId });
+        AnsiConsole.MarkupLine("[green]Successfully updated item[/]");
     }
+    // Delete a flashcard based on id 
 }
