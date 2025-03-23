65fbb817dd0cad97e3f910343df313709f4b3989
Jesus Castro <castrojesus39@gmail.com>
2025-03-22 23:14:09 -0700

    StudySession file created, just notes no implementation

---
+++ Services/StudySessionService.cs
@@ -0,0 +1,8 @@
+﻿namespace FlashcardSimulator.Services;
+
+public class StudySessionService
+{
+    // Get Study sessions from Database
+    // Show all the Study Sessions (basically output to console)
+    // Insert into database using DTO
+}
