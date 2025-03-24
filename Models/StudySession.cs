93a8187a0ede124289193e5c93a706eea19e7091
Jesus Castro <castrojesus39@gmail.com>
2025-03-23 22:43:51 -0700

    model for studysession

---
+++ Models/StudySession.cs
@@ -0,0 +1,13 @@
+﻿namespace FlashcardSimulator.Models;
+
+public class StudySession
+{
+    public int FlashcardId { get; set; }
+    public int StackId { get; set; }
+    public DateTime Date { get; set; }
+    public int AmountOfQuestions { get; set; }
+    public int CorrectAnswersAmount { get; set; }
+    public int Percentage { get; set; }
+    public TimeSpan Time { get; set; }
+    
+}
